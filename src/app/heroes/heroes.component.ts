75817dfc7be792c0ddbb1ad72be3dcaae8ce1aa2
craftbeer <craftbeer@example.com>
2021-02-25 16:57:13 -0500

    tutorial 1.4 (add services) completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 
 @Component({
@@ -16,16 +18,23 @@ export class HeroesComponent implements OnInit {
   };
 
   selectedHero?: Hero;
+  heroes: Hero[] = [];
   
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
-  heroes = HEROES;
-
-  constructor() { }
-
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes():void {
+    //this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
